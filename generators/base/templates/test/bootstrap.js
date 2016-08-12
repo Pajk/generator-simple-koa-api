ef8619fff30bea2f60c8c3db67b7b722826e9a5c
Pavel Pokorny <pajkycz@gmail.com>
2016-08-12 02:39:45 +0200

    configure istanbul to generate coverage report
    + allow to run only specific test file

--- generators/base/templates/test/bootstrap.js
+++ generators/base/templates/test/bootstrap.js
@@ -1,39 +1,29 @@
 /* eslint-disable no-undef, arrow-parens, no-console, global-require */
+require('dotenv').load({ path: '.env-test', silent: true })
 
-before(() => {
-    global.Promise = require('bluebird')
-    Promise.onPossiblyUnhandledRejection(() => {
-        // maybe good for debuggin purposes, but it fires everytime
-        // a promise is rejected eg. in controller and it's not immediately catched
-        // even though it is catched higher in the stack by error middleware
-        // console.log('Rejected Promise: ', err.message)
-    })
-
-    const supertest = require('supertest-as-promised')
-    // const testRunner = require('bandage-runner')
-    const sinon = require('sinon')
-    const assert = require('assert')
-
-    const hashHelper = require('../src/helper/hash')
-    const testHelper = require('./helper')
-    const api = require('../src/api')
-
-    // stub password hashing
-    sinon.stub(hashHelper, 'get', pass => Promise.resolve(pass))
-    sinon.stub(hashHelper, 'verify', (stored, pass) => Promise.resolve(pass === stored))
-
-    api.app.on('error', console.log)
-
-    global.request = supertest(api.app.callback())
-    global.helper = testHelper(global.request)
-    global.assert = assert
+global.Promise = require('bluebird')
+Promise.onPossiblyUnhandledRejection(() => {
+    // maybe good for debuggin purposes, but it fires everytime
+    // a promise is rejected eg. in controller and it's not immediately catched
+    // even though it is catched higher in the stack by error middleware
+    // console.log('Rejected Promise: ', err.message)
 })
 
-after(() => {
-    console.log('AFTER')
-    const sequelize = require('../src/model').sequelize
-    const db = require('../src/helper/db')
+const supertest = require('supertest-as-promised')
+// const testRunner = require('bandage-runner')
+const sinon = require('sinon')
+const assert = require('assert')
 
-    db.disconnect()
-    sequelize.close()
-})
+const hashHelper = require('../src/helper/hash')
+const testHelper = require('./helper')
+const api = require('../src/api')
+
+// stub password hashing
+sinon.stub(hashHelper, 'get', pass => Promise.resolve(pass))
+sinon.stub(hashHelper, 'verify', (stored, pass) => Promise.resolve(pass === stored))
+
+api.app.on('error', console.log)
+
+global.request = supertest(api.app.callback())
+global.helper = testHelper(global.request)
+global.assert = assert
--- generators/base/templates/test/env.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').load({ path: '.env-test', silent: true })
-
-require('babel-core/register')({
-    presets: ['es2015-node6', 'stage-3']
-})
